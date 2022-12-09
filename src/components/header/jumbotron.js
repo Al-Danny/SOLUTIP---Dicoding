8f6538d6a41bbcae11cf05228366ec5889d08e0f
syaahrul31 <syahrull0505@gmail.com>
2022-12-09 08:40:23 +0700

    jumbotron.js jumbotron2.js navbar2.js

---
+++ src/components/header/jumbotron.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import img from '../../assets/image/img-hero.png';
+import antre from '../../assets/image/img-antre.png';
+import konsul from '../../assets/image/img-konsultasi.png';
+import '../../style/jumbotron.css';
+
+function Jumbotron() {
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+
+	return (
+		<>
+			<header>
+				<div className='img-header'>
+					<img src={img} />
+				</div>
+				<div className='hero-content'>
+					<h1>SOLUSI MEDIS TERPERCAYA</h1>
+					<p>
+						Ambil nomor antrean dan dapatkan solusi medismu secara praktis hanya
+						di Solutip Medical Check Up. Stay Health :)
+					</p>
+					<div className='fitur'>
+						<ul>
+							<NavLink
+								to='/antrean'
+								className='klik-antre'
+								onClick={handleClick}
+							>
+								<li>
+									<div className='pic antre'>
+										<img src={antre} />
+									</div>
+									<p>Antrean</p>
+								</li>
+							</NavLink>
+
+							<NavLink
+								to='/login'
+								className='klik-konsultasi'
+								onClick={handleClick}
+							>
+								<li>
+									<div className='pic'>
+										<img src={konsul} />
+									</div>
+									<p>Konsultasi</p>
+								</li>
+							</NavLink>
+						</ul>
+					</div>
+				</div>
+			</header>
+		</>
+	);
+}
+
+export default Jumbotron;
---
+++ src/components/header/navbar2.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/image/logo.png';
+import { BiExit } from 'react-icons/bi';
+import AuthedUserContext from '../../contexts/AuthedUserContext';
+import '../../style/navbar.css';
+import '../../style/hamburger.css';
+
+function Navbar2() {
+	const [click, setClick] = useState(false);
+
+	const handleClick = () => setClick(!click);
+	const { authedUser, onLogout } = React.useContext(AuthedUserContext);
+
+	return (
+		<>
+			<nav className='navbar'>
+				<div className='nav-container'>
+					<NavLink to='/' className='nav-logo'>
+						<img src={Logo} alt='solutip'></img>
+					</NavLink>
+
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						<li className='nav-item'>
+							<NavLink
+								to='/home'
+								activeclassname='active'
+								className='nav-NavLinks'
+								onClick={handleClick}
+							>
+								Home
+							</NavLink>
+						</li>
+						<li className='nav-item'>
+							<NavLink
+								to='/consultation'
+								activeclassname='active'
+								className='nav-NavLinks'
+								onClick={handleClick}
+							>
+								konsultasi
+							</NavLink>
+						</li>
+						<li id='autentifikasi'>
+							{authedUser ? (
+								<button className='button-logout' onClick={onLogout}>
+									{authedUser.name} <BiExit />
+								</button>
+							) : (
+								''
+							)}
+						</li>
+					</ul>
+
+					<div className='nav-icon' onClick={handleClick}>
+						<i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default Navbar2;
