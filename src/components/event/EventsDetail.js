5570bead610605b8b9c4c2f856993481e704e76a
syaahrul31 <syahrull0505@gmail.com>
2022-12-09 20:21:22 +0700

    EventDetail.js

--- src/components/event/EventsDetail.js
+++ src/components/event/EventsDetail.js
@@ -1,25 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { showFormattedDate } from "../../utils/index";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { showFormattedDate } from '../../utils/index';
 
 function EventsDetail({ image, title, createdAt, description }) {
-  return (
-    <>
-        <section className="">
-          <img src={image} />
-          <h3 className="">{title}</h3>
-          <p className="">{showFormattedDate(createdAt)}</p>
-          <div className="">{description}</div>
-        </section>
-    </>
-  )
+	return (
+		<>
+			<section className=''>
+				<img src={image} alt='' />
+				<h3 className=''>{title}</h3>
+				<p className=''>{showFormattedDate(createdAt)}</p>
+				<div className=''>{description}</div>
+			</section>
+		</>
+	);
 }
 
 EventsDetail.propTypes = {
-  image: PropTypes.any,
-  title: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-}
+	image: PropTypes.any,
+	title: PropTypes.string.isRequired,
+	createdAt: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+};
 
 export default EventsDetail;
