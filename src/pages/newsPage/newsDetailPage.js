280db70028ce44443084abe9beb45a57218e0b1e
Syahrul Hidayat <97028064+syaahrul31@users.noreply.github.com>
2022-12-10 06:57:51 +0700

    newsDetailPage
    newsDetailPage

---
+++ src/pages/newsPage/newsDetailPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewsItemDetails from '../../components/news/NewsItemDetail';
+
+function NewsDetailPage() {
+	return (
+		<>
+			<NewsItemDetails />
+		</>
+	);
+}
+
+export default NewsDetailPage;
