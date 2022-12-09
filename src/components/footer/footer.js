19db4f2cbfcefc8c1cb6d6fec8bffd3f92081d0e
syaahrul31 <syahrull0505@gmail.com>
2022-12-09 08:24:18 +0700

    footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,19 +1,50 @@
-import React from "react";
-import "../../style/footer.css";
-import { BsTwitter, BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import React from 'react';
+import '../../style/footer.css';
+import { BsTwitter, BsFacebook, BsInstagram, BsYoutube } from 'react-icons/bs';
 
-function Footer () {
-
-  return (
-      <footer>
-        <div className="down">
-          <a href="#"><p className="cpy">&copy;SOLUTIP, 2022. ALL RIGHTS RESERVED</p></a>
-          <div className="follow">
-            <p className="flw">Follow kami di  <a href="#"><BsTwitter /></a><a href="#"><BsFacebook /></a><a href="#"><BsInstagram /></a><a href="#"><BsYoutube /></a></p>
-          </div>
-        </div>
-      </footer>
-  )
+function Footer() {
+	return (
+		<footer>
+			<div className='copyright'>
+				<a href='#nav'>
+					<p className='cpy'>&copy;SOLUTIP, 2022. ALL RIGHTS RESERVED</p>
+				</a>
+			</div>
+			<div className='follow'>
+				<p className='flw'>FOLLOW KAMI DI </p>
+				<div className='contacts'>
+					<a
+						href='https://twitter.com/ukki_upnjatim'
+						target='_blank'
+						rel='no noreferrer'
+					>
+						<BsTwitter />
+					</a>
+					<a
+						href='https://web.facebook.com/upn.ukki'
+						target='_blank'
+						rel='no noreferrer'
+					>
+						<BsFacebook />
+					</a>
+					<a
+						href='https://www.instagram.com/ukki_upn/'
+						target='_blank'
+						rel='no noreferrer'
+					>
+						<BsInstagram />
+					</a>
+					<a
+						href='https://www.youtube.com/@ukkiupnveteranjawatimur7001'
+						target='_blank'
+						rel='no noreferrer'
+					>
+						<BsYoutube />
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
