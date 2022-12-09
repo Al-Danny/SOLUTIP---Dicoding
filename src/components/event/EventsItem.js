ac3f48ce60684b2dcfb9cf8b633e3ba1e6c8c019
syaahrul31 <syahrull0505@gmail.com>
2022-12-09 20:46:19 +0700

    EventsItem.js event-data.js

--- src/components/event/EventsItem.js
+++ src/components/event/EventsItem.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { showFormattedDate } from "../../utils/index";
+import React from 'react';
+import { showFormattedDate } from '../../utils/index';
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import '../../style/contentHome.css';
 
 function EventsItem({ id, image, title, createdAt, description }) {
 	return (
-		<article>
-			<img src={image} alt={title} />
-			<div className='description'>
-				<h2>
-				<Link to={`/event/${id}`}>{title}</Link>
-				</h2>
-				<p>{showFormattedDate(createdAt)}</p>
-				<p className=''>{description.slice(0, 100)}...</p>
-			</div>
-		</article>
+		<Link to={`/event/${id}`}>
+			<article>
+				<img src={image} alt={title} />
+				<div className='description'>
+					<h2>{title}</h2>
+					<p>{showFormattedDate(createdAt)}</p>
+					<p className=''>{description.slice(0, 100)}...</p>
+				</div>{' '}
+				<br />
+			</article>
+		</Link>
 	);
 }
 
